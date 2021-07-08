7e71267a71800482c3ca395a0c78499ce504cf84
SirniiK <86946026+SirniiK@users.noreply.github.com>
2021-07-08 16:46:54 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,24 @@
 
+import re
+
+spisokn = []
+spisokw = []
+
+Input = "Hel112o, wor24ld o0f pyt73hon1".split()
+for elem in Input:
+	spisokn.extend(re.findall('(\d+)', elem))
+
+for elem in Input:
+	spisokw.append("".join(re.findall('(\D+)', elem)))
+
+for i in range(len(spisokn)):
+	spisokn[i] = int(spisokn[i])
+
+for i in range(len(spisokn)):
+	if spisokn[i] != max(spisokn):
+		print(pow(spisokn[i], i))
+print(spisokn)
+
+for i in range(len(spisokw)):
+	spisokw[i] = spisokw[i].title()[0:-1]+spisokw[i][-1].upper()
+print(" ".join(spisokw))
